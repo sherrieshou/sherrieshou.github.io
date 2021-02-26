b93391d14f6b0857fb277c3ff521124f557f11b8
sherrieshou <60146924+sherrieshou@users.noreply.github.com>
2021-02-26 16:11:00 +0800

    Added 2 projects

---
+++ project2/js/page5.js
@@ -0,0 +1,17 @@
+var a;
+function show_hide()
+
+{
+    if(a==1)
+    {
+        document.getElementById("hide").style.display="inline";
+        return a=0;
+    }
+
+    else
+    {
+        document.getElementById("hide").style.display="none";
+        return a=1;
+    }
+
+}
