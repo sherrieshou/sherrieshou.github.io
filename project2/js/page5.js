29bafacd46e4a55d0cee7645c2adf684ff683f7e
sherrieshou <60146924+sherrieshou@users.noreply.github.com>
2021-03-02 10:02:34 +0800

    Fixed mobile screen

--- project2/js/page5.js
+++ project2/js/page5.js
@@ -15,3 +15,43 @@ function show_hide()
     }
 
 }
+
+
+
+var a;
+function show_hide2()
+
+{
+    if(a==1)
+    {
+        document.getElementById("hide2").style.display="inline";
+        return a=0;
+    }
+
+    else
+    {
+        document.getElementById("hide2").style.display="none";
+        return a=1;
+    }
+
+}
+
+
+
+var a;
+function show_hide3()
+
+{
+    if(a==1)
+    {
+        document.getElementById("hide3").style.display="inline";
+        return a=0;
+    }
+
+    else
+    {
+        document.getElementById("hide3").style.display="none";
+        return a=1;
+    }
+
+}
