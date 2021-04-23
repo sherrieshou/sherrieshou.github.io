7b6be3652cc966e39f15b8867be975459953707e
sherrieshou <60146924+sherrieshou@users.noreply.github.com>
2021-04-23 19:58:18 +0800

    Submit Assignment 5

--- apr16/script.js
+++
@@ -1,7 +0,0 @@
-console.log("inside");
-
-$("div").click(function(){
-    var color = $(this).css("background-color");
-    $("#result").html("That div is <span style='color:" +
-    color + ";'>" + color + "</span>.")
-})
