d3f66140f2f0d15bee316d77c9726edf1c3bd4fd
sherrieshou <60146924+sherrieshou@users.noreply.github.com>
2021-05-01 00:35:41 +0800

    Update

--- project4/script.js
+++ project4/script.js
@@ -1,3 +1,7 @@
+//When you can't decide where to go for weekend, you can use this webpage to help you
+//You will first need to select an area 
+
+//Here are the 4 buttons that generates different cards
 //Haidian Random Images
 let hdDiv = document.querySelector("#hd");
 
@@ -51,15 +55,45 @@ document.getElementById('images').src = dcimages[rnd];
 
   //XiCheng Random Images
   let xcDiv = document.querySelector("#xc");
-
+ 
 // Click Event - detect when this div has been clicked
 xcDiv.addEventListener("click", function(event){
+ //If the circle is clicked, add append text box to that button say click again for more
+ //grab the element by id text and append inner html "Click again if you don't like the result"
+ 
+if (sessionStorage.getItem('imgSrc')) {
+  document.getElementById('images').src = sessionStorage.getItem('imgSrc');
+}
+
+
 
 var xcimages = ["img/4pm.png", "img/5am.png", "img/7pm.png", ];
  // create the links arrey and name it hdLinks
 //  var hdLinks = new Array();
-
 var rnd = Math.floor(Math.random() * xcimages.length);
 
 document.getElementById('images').src = xcimages[rnd];
+
+sessionStorage.setItem('imgSrc', xcimages[rnd]);
+
   });
+
+  let hintDiv = document.querySelector("#xc,#dc,#hd,#cy");
+  let textDiv = document.querySelector("#text")
+
+  hintDiv.addEventListener("click", function(event){
+    if (textDiv.classList.contains('hidden')) {
+      textDiv.classList.remove('hidden');
+      sessionStorage.setItem('textIsVisible', 'true');
+    } else {
+      textDiv.classList.add('hidden');
+      sessionStorage.removeItem('textIsVisible');
+    }
+    if (sessionStorage.getItem('textIsVisible')) {
+      textDiv.classList.remove('hidden');
+    }
+  });
+  
+  
+
+
