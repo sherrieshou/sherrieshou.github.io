69f87bd2cc9059ea518b3cab04e008091c27d97e
sherrieshou <60146924+sherrieshou@users.noreply.github.com>
2022-10-02 17:36:26 -0400

    quietQuitting

--- quietquitting/script.js
+++ quietquitting/script.js
@@ -1,54 +1,160 @@
+var top = this.scrollY;
+
+var up =  ["-88", "-62", "-38", "-12","12", "38", "62" ]
+
+var up2 =  ["-88", "-62", "-38", "-12","12", "38", "62" ]
+
+var up3 =  ["-88", "-62", "-38", "-12","12", "38", "62" ]
+
+var up4 =  ["-88", "-62", "-38", "-12","12", "38", "62" ]
+
+var up5 =  ["-88", "-62", "-38", "-12","12", "38", "62" ]
+
+var up6 =  ["-88", "-62", "-38", "-12","12", "38", "62" ]
+
+var rndPos1 = Math.floor(Math.random()* up.length);
+
+var rndPos2 = Math.floor(Math.random()* up.length);
+
+var rndPos3 = Math.floor(Math.random()* up.length);
+
+var rndPos4 = Math.floor(Math.random()* up.length);
+
+var rndPos5 = Math.floor(Math.random()* up.length);
+
+var rndPos6 = Math.floor(Math.random()* up.length);
+
+var images = ["img/events/box1.png", "img/events/box2.png", "img/events/box3.png", "img/events/box4.png", "img/events/box5.png", "img/events/box6.png",]
+
+var images2 = ["img/events/box21.png", "img/events/box22.png", "img/events/box23.png", "img/events/box24.png", "img/events/box25.png", "img/events/box26.png",]
+
+var images3 = ["img/events/box31.png", "img/events/box32.png", "img/events/box33.png", "img/events/box34.png", "img/events/box35.png", "img/events/box36.png",]
+
+var images4 = ["img/events/box41.png", "img/events/box42.png", "img/events/box43.png", "img/events/box44.png", "img/events/box45.png", "img/events/box46.png",]
+
+var images5 = ["img/events/box51.png", "img/events/box52.png", "img/events/box53.png", "img/events/box54.png", "img/events/box55.png", "img/events/box56.png",]
+
+var images6 = ["img/events/box61.png", "img/events/box62.png", "img/events/box63.png", "img/events/box64.png", "img/events/box65.png", "img/events/box66.png",]
+
+var rnd = Math.floor(Math.random()* images.length);
+
+var rnd2 = Math.floor(Math.random()* images.length);
+
+var rnd3 = Math.floor(Math.random()* images.length);
+
+var rnd4 = Math.floor(Math.random()* images.length);
+
+var rnd5 = Math.floor(Math.random()* images.length);
+
+var rnd6 = Math.floor(Math.random()* images.length);
 
 
-var x = [];
-var y = [];
-var w = 30;
-var col = [];
+// scroll1
+myBox1 = document.getElementById("myBox1");
+var myFirScroll = function () {
+    var y = window.scrollY;
+    if (y >= 100) {
+        myBox1.className = "bottomMenu show"
+        // console.log(images[rnd])
+        document.getElementById('images1').src = images[rnd];
+        myBox1.style.left = up[rndPos1] + '%';
 
-function setup() {
-    createCanvas(600, 858);
-    for (var i = 0; i < 30; i++) {
-        x[i] = w + i * w;
-        y[i] = w + i * w;
+    } else {
+        myBox1.className = "bottomMenu hide"
     }
 
-    ///////// assign a color for all 100 squares
-    for (var i = 0; i < 300; i++) {
-        col[i] = true;
-    }
-    
-}
 
-///////////// (j * 10 + i) = row number plus x position
-function draw() {
-  
-   const columns = 7;
-        const rows = 24;
-        const cellWidth = width / columns;
-        const cellHeight = height / rows;
-   for (let c = 0; c < columns; c++) {
-          for (let r = 0; r < rows; r++) {
-            const x = c * cellWidth + cellWidth / 2;
-            const y = r * cellHeight + cellHeight / 2;
-          }}
-  
-    background(0);
-    rectMode(CENTER);
-    stroke(0);
-    for (var j = 0; j < y.length; j++) {
-        for (var i = 0; i < 10; i++) {
-            if (col[j * 10 + i]) fill("white");
-            else fill("pink");
-            rect(x[i], y[j], w, w);
-        }
-    }
-}
+};
+window.addEventListener("scroll", myFirScroll);
 
-function mousePressed() {
-    for (var j = 0; j < 100; j++) {
-        for (var i = 0; i < 10; i++) {
-            var dis = dist(mouseX, mouseY, x[i], y[j]);
-            if(dis < w/2) col[j * 10 + i] =! col[j * 10 + i];
-        }
+// scroll2
+myBox2 = document.getElementById("myBox2");
+var mySecScroll = function () {
+    var y = window.scrollY;
+    if (y >= 300) {
+        myBox2.className = "bottomMenu show"
+        document.getElementById('images2').src = images2[rnd2];
+        myBox2.style.left = up2[rndPos2] + '%';
+
+    } else {
+        myBox2.className = "bottomMenu hide"
     }
-}
+
+
+};
+
+window.addEventListener("scroll", mySecScroll);
+
+// scroll3
+myBox3 = document.getElementById("myBox3");
+var myTrdScroll = function () {
+    var y = window.scrollY;
+    if (y >= 800) {
+        myBox3.className = "bottomMenu show"
+        document.getElementById('images3').src = images3[rnd3];
+        myBox3.style.left = up3[rndPos3] + '%';
+
+    } else {
+        myBox3.className = "bottomMenu hide"
+    }
+
+
+};
+
+window.addEventListener("scroll", myTrdScroll);
+
+
+// scroll4
+myBox4 = document.getElementById("myBox4");
+var myForScroll = function () {
+    var y = window.scrollY;
+    if (y >= 1300) {
+        myBox4.className = "bottomMenu show"
+        document.getElementById('images4').src = images4[rnd4];
+        myBox4.style.left = up4[rndPos4] + '%';
+
+    } else {
+        myBox4.className = "bottomMenu hide"
+    }
+
+
+};
+
+window.addEventListener("scroll", myForScroll);
+
+
+// scroll5
+myBox5 = document.getElementById("myBox5");
+var myFivScroll = function () {
+    var y = window.scrollY;
+    if (y >= 1500) {
+        myBox5.className = "bottomMenu show"
+        document.getElementById('images5').src = images5[rnd5];
+        myBox5.style.left = up5[rndPos5] + '%';
+    } else {
+        myBox5.className = "bottomMenu hide"
+    }
+
+
+};
+
+window.addEventListener("scroll", myFivScroll);
+
+
+// scroll6
+myBox6 = document.getElementById("myBox6");
+var mySixScroll = function () {
+    var y = window.scrollY;
+    if (y >= 1900) {
+        myBox6.className = "bottomMenu show"
+        document.getElementById('images6').src = images6[rnd6];
+        myBox6.style.left = up6[rndPos6] + '%';
+    } else {
+        myBox6.className = "bottomMenu hide"
+    }
+
+
+};
+
+window.addEventListener("scroll", mySixScroll);
+
